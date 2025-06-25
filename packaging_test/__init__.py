aaa58368caf2112514076bc750dafa3841fee5ed
Ariel Kanterewicz (BF) <akanterewicz@baufest.com>
2025-06-25 19:02:04 +0000

    stuff

---
+++ packaging_test/__init__.py
@@ -0,0 +1,9 @@
+# /// script
+# requires-python = ">=3.10"
+# dependencies = [
+#   "slack-sdk==3.35.0",
+# ]
+# ///
+
+if __name__ == "__main__":
+    print("This script is designed to be run as a module, not directly.")
